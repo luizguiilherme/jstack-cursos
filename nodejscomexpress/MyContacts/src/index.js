926332a4fab7213825d2fc96b069a8c93590623d
Luiz Guilherme <luifome@outlook.com>
2022-07-08 18:28:34 -0300

    organizando a pasta e iniciando o server com o express

---
+++ nodejscomexpress/MyContacts/src/index.js
@@ -0,0 +1,10 @@
+const { response } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('Hello World');
+});
+
+app.listen(3000,() => console.log('🐗️ Server started at http://localhost:3000'));
