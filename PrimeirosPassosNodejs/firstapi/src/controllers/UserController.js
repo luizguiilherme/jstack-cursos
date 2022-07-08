45a514d6825b66e22bc60e54c1c64a1a18fcafdc
Luiz Guilherme <luifome@outlook.com>
2022-07-08 16:25:40 -0300

    Criando helper bodyParser
    Executa automaticamente uma requisição dos tipos PUT ou POST

--- PrimeirosPassosNodejs/firstapi/src/controllers/UserController.js
+++ PrimeirosPassosNodejs/firstapi/src/controllers/UserController.js
@@ -31,22 +31,13 @@ module.exports = {
   },
 
   createUser(request, response){
-    let body = '';
-
-    request.on('data', (chunck) =>{
-      body += chunck
-    });
-
-    request.on('end', () => {
-      body = JSON.parse(body);
-
-      const lastUserId = users[users.length - 1].id;
-      const newUser = {
-        id: lastUserId + 1,
-        name: body.name,
-      };
-      users.push(newUser);
-      response.send(200, newUser);
-    });
+    const { body } = request;
+    const lastUserId = users[users.length - 1].id;
+    const newUser = {
+      id: lastUserId + 1,
+      name: body.name,
+    };
+    users.push(newUser);
+    response.send(200, newUser);
   },
 };
