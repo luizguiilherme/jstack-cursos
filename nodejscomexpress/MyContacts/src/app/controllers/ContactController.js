04c2397a5b2ff4825c740c45ca5e935225aa0f04
Luiz Guilherme <luifome@outlook.com>
2022-07-13 18:49:11 -0300

    Refatorando meu Repositório
    Fiz alterações no meu repositório para interagir com meu banco de dados

--- nodejscomexpress/MyContacts/src/app/controllers/ContactController.js
+++ nodejscomexpress/MyContacts/src/app/controllers/ContactController.js
@@ -1,9 +1,10 @@
 const ContactsRepository = require('../repositories/ContactsRepository');
 
 class ContactController {
-  async index(require, response) {
+  async index(request, response) {
   // Listar todos os registros
-    const contacts = await ContactsRepository.findAll();
+    const { orderBy } = request.query;
+    const contacts = await ContactsRepository.findAll(orderBy);
 
     response.json(contacts);
   }
@@ -75,13 +76,6 @@ class ContactController {
   async delete(request, response) {
     // Deletar um registro
     const { id } = request.params;
-
-    const contact = await ContactsRepository.findById(id);
-
-    if (!contact) {
-      // 404: Not Found
-      return response.status(404).json({ error: 'User not found' });
-    }
 
     await ContactsRepository.delete(id);
     // 204: No Content
