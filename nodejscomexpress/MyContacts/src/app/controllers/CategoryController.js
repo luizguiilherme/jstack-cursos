3435e91802f7f73d9dc9e1cd756f572676c1ba1e
Luiz Guilherme <luifome@outlook.com>
2022-07-14 15:07:59 -0300

    Criando métodos e adicionando rotas  na minha categorias

--- nodejscomexpress/MyContacts/src/app/controllers/CategoryController.js
+++ nodejscomexpress/MyContacts/src/app/controllers/CategoryController.js
@@ -19,6 +19,45 @@ class CategoryController {
 
     response.json(category);
   }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const category = await CategoriesRepository.findById(id);
+
+    if (!category) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    response.json(category);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const {
+      name,
+    } = request.body;
+
+    const categoryExist = await CategoriesRepository.findById(id);
+    if (!categoryExist) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoriesRepository.update(id, { name });
+
+    response.json(category);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await CategoriesRepository.delete(id);
+    response.sendStatus(204);
+  }
 }
 
 module.exports = new CategoryController();
