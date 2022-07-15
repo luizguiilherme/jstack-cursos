88fd70689585d51c4b0936da1ff58c1658ced3d1
Luiz Guilherme <luifome@outlook.com>
2022-07-15 16:41:50 -0300

    ES Modules

---
+++ javascript/05-esmodules/sayHello.js
@@ -0,0 +1,12 @@
+export function sayHello(name){
+  console.log(`Olá, ${name}`)
+}
+
+// Exportação nomeada
+//export {sayHello};
+
+// Exportação default
+
+const userName = 'Luiz';
+
+export default userName;
